4e175f91d3bef0d4c6400e13c02032663df747bb
JB <96971698+KingBobushcka@users.noreply.github.com>
2022-04-19 17:42:48 -0400

    second commit

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -20,7 +20,7 @@ var speakWord = "Good Bye";
 // See Lecture 52, part 2
 byeSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
-}
+};
 
 window.byeSpeaker = byeSpeaker;
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
--- script.js
+++ script.js
@@ -65,3 +65,5 @@ for (var i = 0; i < names.length; i++) {
 }
 
 })();
+
+
